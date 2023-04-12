392cf888a2e285a47a176c6a71d93f46c71e80b4
Kenneth <nitramnek@gmail.com>
2023-04-12 21:09:06 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+//k3n5c0d3
+// Code is here
+const num1=31
+const num2=2
+const num3=9
+const num4=20
+//Assign the variables
 
-// Write your code here
+const multiply =num1*num2
+
+const random = Math.round(Math.random() * 100) + 1;
+console.log (multiply);
+
+const mod= num1 % num3;
+console.log (mod);
+
+const max = Math.max(num2,num3,num4)
+console.log(max)
